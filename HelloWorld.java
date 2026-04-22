1693317174af98b218726dd325b777468d66649e
Vishwajeet Singh <vishwajeetsingh_cool@hotmail.com>
2026-04-23 01:52:59 +0530

    Update greeting message in HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Prince and yogesh , jay from batch-43!");
+        System.out.println("Hello, Prince and yogesh , jay from batch-43 from CloudDevOpsHub!");
     }
 }
